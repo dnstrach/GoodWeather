40e59e4f550214d925318d70669a4e5a90cce1cd
dnstrach <dnstrach@gmail.com>
2023-06-26 22:08:45 -0700

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Dominique Strachan on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
